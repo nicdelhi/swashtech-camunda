1a640b0ea7637e9831b8acd76df80fa9623ac5cf
Ashish Sirsikar <asirsikar319@gmail.com>
2020-06-28 09:17:42 +0530

    First Commit

---
+++ src/test/java/com/swashtech/SwashtechCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swashtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwashtechCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
