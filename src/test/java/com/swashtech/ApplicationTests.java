62a4be9e75e4ec2824b50be43a16b11686454050
Ashish Sirsikar <asirsikar319@gmail.com>
2021-04-24 10:25:54 +0530

    jersey configuration added

--- src/test/java/com/swashtech/ApplicationTests.java
+++ src/test/java/com/swashtech/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SwashtechCamundaApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
